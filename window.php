132b665b8eaedd0f3fdd4cd903a23361f11cd654
naumov <naumov@agima.ru>
2016-09-29 20:42:10 +0300

    init repository '

---
+++ window.php
@@ -0,0 +1,15 @@
+<?php
+  $gtk = new Gtk;
+//  $callback = callback();
+  $gtk
+    ->setTitle("testing windows for habr")
+    ->setButtonTittle("testing")
+    ->createWindow()
+    ->setButton()
+//    ->setCallback(function(){echo "testing"; })
+    ->render();
+  
+//  var_dump($gtk->checkButtonPress());
+//function callback() {
+//  echo "testing huesting mesting";
+//}
